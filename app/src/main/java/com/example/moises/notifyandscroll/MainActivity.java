cea6fce5c27d867447c5980b3fe1eb1b719890a8
Moisés Mena <Moiss Mena>
2016-09-28 15:22:18 -0600

    Tecnicas de scroll terminadas

---
+++ app/src/main/java/com/example/moises/notifyandscroll/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.moises.notifyandscroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//fin del constructor
+}//fin de la clase
